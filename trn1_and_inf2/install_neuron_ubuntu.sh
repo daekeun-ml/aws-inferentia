63c60caa8e6b55038a7904733a1f7048fd4309e0
daekeun-ml <daekeun@amazon.com>
2023-02-17 08:33:29 +0900

    20230217 - Modified setup scripts

--- trn1_and_inf2/install_neuron_ubuntu.sh
+++ trn1_and_inf2/install_neuron_ubuntu.sh
@@ -1,71 +1,66 @@
 # Configure Linux for Neuron repository updates
-
-sudo tee /etc/yum.repos.d/neuron.repo > /dev/null <<EOF
-[neuron]
-name=Neuron YUM Repository
-baseurl=https://yum.repos.neuron.amazonaws.com
-enabled=1
-metadata_expire=0
+. /etc/os-release
+sudo tee /etc/apt/sources.list.d/neuron.list > /dev/null <<EOF
+deb https://apt.repos.neuron.amazonaws.com ${VERSION_CODENAME} main
 EOF
-sudo rpm --import https://yum.repos.neuron.amazonaws.com/GPG-PUB-KEY-AMAZON-AWS-NEURON.PUB
+wget -qO - https://apt.repos.neuron.amazonaws.com/GPG-PUB-KEY-AMAZON-AWS-NEURON.PUB | sudo apt-key add -
 
-# Update OS packages
-sudo yum update -y
+# Update OS packages 
+sudo apt-get update -y
 
-# Install git
-sudo yum install git -y
+# Install OS headers 
+sudo apt-get install linux-headers-$(uname -r) -y
 
+# Install git 
+sudo apt-get install git -y
 
-# Install OS headers
-sudo yum install kernel-devel-$(uname -r) kernel-headers-$(uname -r) -y
+# install Neuron Driver
+sudo apt-get install aws-neuronx-dkms=2.* -y
 
-# Remove preinstalled packages and Install Neuron Driver and Runtime
-sudo yum remove aws-neuron-dkms -y
-sudo yum remove aws-neuronx-dkms -y
-sudo yum remove aws-neuronx-oci-hook -y
-sudo yum remove aws-neuronx-runtime-lib -y
-sudo yum remove aws-neuronx-collectives -y
-sudo yum install aws-neuronx-dkms-2.*  -y
-sudo yum install aws-neuronx-oci-hook-2.*  -y
-sudo yum install aws-neuronx-runtime-lib-2.*  -y
-sudo yum install aws-neuronx-collectives-2.*  -y
+# Install Neuron Runtime 
+sudo apt-get install aws-neuronx-collectives=2.* -y
+sudo apt-get install aws-neuronx-runtime-lib=2.* -y
 
-# Install EFA Driver(only required for multiinstance training)
-curl -O https://efa-installer.amazonaws.com/aws-efa-installer-latest.tar.gz
-wget https://efa-installer.amazonaws.com/aws-efa-installer.key && gpg --import aws-efa-installer.key
-cat aws-efa-installer.key | gpg --fingerprint
-wget https://efa-installer.amazonaws.com/aws-efa-installer-latest.tar.gz.sig && gpg --verify ./aws-efa-installer-latest.tar.gz.sig
-tar -xvf aws-efa-installer-latest.tar.gz
-cd aws-efa-installer && sudo bash efa_installer.sh --yes
-cd
-sudo rm -rf aws-efa-installer-latest.tar.gz aws-efa-installer
+# Install EFA Driver (only required for multi-instance training)
+curl -O https://efa-installer.amazonaws.com/aws-efa-installer-latest.tar.gz 
+wget https://efa-installer.amazonaws.com/aws-efa-installer.key && gpg --import aws-efa-installer.key 
+cat aws-efa-installer.key | gpg --fingerprint 
+wget https://efa-installer.amazonaws.com/aws-efa-installer-latest.tar.gz.sig && gpg --verify ./aws-efa-installer-latest.tar.gz.sig 
+tar -xvf aws-efa-installer-latest.tar.gz 
+cd aws-efa-installer && sudo bash efa_installer.sh --yes 
+cd 
+sudo rm -rf aws-efa-installer-latest.tar.gz aws-efa-installer 
 
-# Remove pre-installed package and Install Neuron Tools
-sudo yum remove aws-neuron-tools  -y
-sudo yum remove aws-neuronx-tools  -y
-sudo yum install aws-neuronx-tools-2.*  -y
+# Install Neuron Tools 
+sudo apt-get install aws-neuronx-tools=2.* -y
 
+# Add PATH
 export PATH=/opt/aws/neuron/bin:$PATH
 
+# Install Python venv 
+sudo apt-get install -y python3.8-venv g++ 
 
-# Install Python venv and activate Python virtual environment to install
-# Neuron pip packages.
-python3.7 -m venv aws_neuron_venv_pytorch
-source aws_neuron_venv_pytorch/bin/activate
-pip install -U pip
+# Create Python venv
+python3.8 -m venv aws_neuron_venv_pytorch 
+
+# Activate Python venv 
+source aws_neuron_venv_pytorch/bin/activate 
+python -m pip install -U pip 
 
 # Set pip repository pointing to the Neuron repository 
 python -m pip config set global.extra-index-url https://pip.repos.neuron.amazonaws.com
 
-# Install wget, awscli
-pip install wget
-pip install awscli
+# Install wget, awscli 
+python -m pip install wget 
+python -m pip install awscli 
 
-# Install Neuron packages
-pip install neuronx-cc==2.* 
-pip install torch-neuronx torchvision
+# Install Neuron Compiler
+python -m pip install neuronx-cc==2.* 
+
+# Install Neuron Framework
+python -m pip install torch-neuronx torchvision
 
 # Install additional packages
-pip install transformers==4.25.1
-pip install opencv-python-headless==4.6.0.66
-pip install matplotlib scikit-learn seaborn
+python -m pip install transformers==4.25.1
+python -m pip install opencv-python-headless==4.6.0.66
+python -m pip install matplotlib scikit-learn seaborn
